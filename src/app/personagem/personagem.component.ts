9b15f038f3398f353b5fd0b5dc0b9324e2bf9692
Nicolas-NL <you@example.com>
2023-03-14 11:00:41 -0300

    Inicio

---
+++ src/app/personagem/personagem.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personagem',
+  templateUrl: './personagem.component.html',
+  styleUrls: ['./personagem.component.css']
+})
+export class PersonagemComponent {
+  nome: String = "Luke Skywalker";
+
+}
